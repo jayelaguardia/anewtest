ad6c19fc180904e73ceb5f805389c706ed11b887
Jaye Laguardia <jayelaguardia@gmail.com>
2020-06-17 04:14:42 -0700

    restart

--- src/user/user-router.js
+++ src/user/user-router.js
@@ -29,7 +29,7 @@ userRouter
       if (hasUserWithUserName)
         return res.status(400).json({ error: `Username already taken` });
 
-      const hashedPassword = await UserService.hashPassword(password);
+      const hashedPassword = await  UserService.hashPassword(password);
 
       const newUser = {
         username,
@@ -42,9 +42,14 @@ userRouter
         newUser
       );
 
+      // await UserService.populateUserWords(
+      //   req.app.get('db'),
+      //   user.id
+      // );
+
       res
         .status(201)
-        .location(path.posix.join(req.originalUrl, `/${user.id}`))
+        // .location(path.posix.join(req.originalUrl, `/${user.id}`))
         .json(UserService.serializeUser(user));
     } catch(error) {
       next(error);
